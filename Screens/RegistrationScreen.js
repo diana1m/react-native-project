0ccdc98e9597bbcd0d811991adb6c53ae5844b64
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-07-21 16:36:25 +0300

    change style for image

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, ImageBackground, TextInput, View, Text } from "react-native";
+import { StyleSheet, ImageBackground, TextInput, View, Text, Dimensions } from "react-native";
 import BGImage from "./../Images/bg.png"
 import { Button} from "react-native-elements";
 
@@ -7,12 +7,13 @@ export const RegistrationScreen = ()=>{
     const [login, setLogin] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const screenHeight = Dimensions.get("window").height;
 
 
     return(
-        <View style={{backgroundColor: "grey"}}>
-            <ImageBackground source={require("./../Images/bg.png")} resizeMode="cover" />
-            <View style={{marginTop: 200, backgroundColor: "#fff", borderTopLeftRadius: 25, borderTopRightRadius: 25, padding: 16 }}>
+        <View style={{ flex: 1, height: "100%"}}>
+            <ImageBackground source={require("../Images/bg.png")} style={styles.image}/>
+            <View style={styles.layout}>
                 <Text style={styles.title}>
                     Реєстрація
                 </Text>
@@ -28,35 +29,48 @@ export const RegistrationScreen = ()=>{
 }
 
 const styles = StyleSheet.create({
-      title: {
-        color: "#212121",
-        textAlign: "center",
-        fontFamily: "Roboto",
-        fontSize: 30,
-        fontWeight: 500,
-        letterSpacing: 0.3,
-        marginBottom: 33,
-        marginTop:92
-      },
-      input:{
-        height: 50,
-        backgroundColor:"#F6F6F6",
-        color: "#212121",
-        borderWidth: 1,
-        borderColor: "#E8E8E8",
-        borderRadius: 8,
-        marginBottom: 16,
-        padding: 16,
-      },
-      btnSubmit: {
-        display: "flex",
-        // paddingTop: 16,
-        paddingHorizontal:16,
-        paddingVertical: 32,
-        flexDirection: "column",
-        alignItems: "center",
-        borderRadius: 100,
-        backgroundColor: "#FF6C00",
-      }
-
+  image: {
+    flex: 1,
+    // justifyContent: 'center',
+    // height: "100%",
+    // width: "100%",
+    height: 1200,
+    backgroundColor: "#8C651E",
+  },
+  layout: {
+    zIndex:10,
+    height: 'auto',
+    marginTop: 200, 
+    backgroundColor: "#fff", 
+    borderTopLeftRadius: 25, 
+    borderTopRightRadius: 25, 
+    padding: 16
+  },
+  title: {
+    color: "#212121",
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontSize: 30,
+    fontWeight: 500,
+    letterSpacing: 0.3,
+    marginBottom: 33,
+    marginTop:92
+  },
+  input:{
+    height: 50,
+    backgroundColor:"#F6F6F6",
+    color: "#212121",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginBottom: 16,
+    padding: 16,
+  },
+  btnSubmit: {
+    display: "flex",
+    paddingHorizontal:16,
+    paddingVertical: 32,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  }
 });
